32bf95fc74cff9efa1c383cc932e71a7057b94e0
negulescum <negulescumihai@gmail.com>
2025-02-14 16:14:04 +0200

    use module for security group

--- main.tf
+++ main.tf
@@ -24,12 +24,30 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-vpc_security_group_ids = [aws_security_group.blog.id]      # Add this security group to our Instance. Give a list of multiple security groups that we want to apply
+vpc_security_group_ids = [module.blog_sg.security_group_id]     # Add this security group to our Instance. Give a list of multiple security groups that we want to apply
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  version = "4.13.0"
+  name    = "blog_new"
+   vpc_id = data.aws_vpc.default.id
+
+   ingress_rules       = ["http-80-tcp" "https-443-tcp"]
+   ingress_cidr_blocks = ["0.0.0.0/0]
+
+egress_rules       = ["all-all"]
+   egress_cidr_blocks = ["0.0.0.0/0]
+
+
+}
+
+
+
 
 resource "aws_security_group" "blog" {    # Security group Block which describes infrastructure security
 name = "blog"                             # the name that will show up in the AWS console
