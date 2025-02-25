96d96550d059e388e083fabadf4107154b047bf5
negulescum <negulescumihai@gmail.com>
2025-02-25 12:47:36 +0200

    new dev vpc

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "blog_vpc" {                                                   # Declares
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]    # Specifies three Availability Zones (eu-west-1a, eu-west-1b, eu-west-1c).
   
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]    #Creates three public subnets, one per AZ.
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]    # Creates three public subnets, one per AZ.
 
   
 
@@ -53,8 +53,9 @@ subnet_id = module.blog_vpc.public_subnets[0]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws//modules/http-80"
   version = "4.13.0"
+  vpc_id = module.blog_vpc.vpc_id
   name    = "blog"
-   vpc_id = module.blog_vpc.vpc_id
+  
 
    ingress_rules       = ["http-80-tcp" , "https-443-tcp"]
    ingress_cidr_blocks = ["0.0.0.0/0"]
