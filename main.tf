1709bf1c097bc3338e46fafbcfee6752581c53c1
negulescum <negulescumihai@gmail.com>
2025-02-26 15:09:55 +0200

    autoscaling

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ module "autoscaling" {                                      # Define the instanc
 
    image_id     = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  target_group_arns = module.blog_alb.target_group_arns
 
   
 }
@@ -80,29 +81,7 @@ module "blog_alb" {
 
 
 
-  # Security Group
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
-    }
-  }
+  
 
 
   listeners = {
